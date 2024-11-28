57603bfb96cbbdbfcb437d86fde23f8c31a5e1b3
LeartBokshi <lb70606@ubt-uni.net>
2024-11-28 15:49:52 +0100

    log in

--- singupscript.js
+++ singupscript.js
@@ -20,14 +20,4 @@ document.getElementById("togglePassword").addEventListener("click", function () 
       this.textContent = "👁"; // Change icon back to show password
     }
   });
-  document.getElementById("ltogglePassword").addEventListener("click", function () {
-    const passwordField = document.getElementById("lpassword");
-  
-    if (passwordField.type === "password") {
-      passwordField.type = "text";
-      this.textContent = "🙈"; // Change icon to hide password
-    } else {
-      passwordField.type = "password";
-      this.textContent = "👁"; // Change icon back to show password
-    }
-  });
+ 
