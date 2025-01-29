2378ec53a1c2423fb0810107f695d09802e158cb
Florent Latifi <120279421+FlorentLatifi@users.noreply.github.com>
2025-01-29 19:28:54 +0100

    q

--- test.php
+++ test.php
@@ -5,8 +5,9 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Image Slider</title>
   <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600&display=swap" rel="stylesheet">
-  <link rel="stylesheet" href="slider.css">
+  
   <link rel="stylesheet" href="navbar.css">
+  <link rel="stylesheet" href="slider.css">
 </head>
 <body>
 <?php require 'nav.php'; ?>
