fa43de15fe22a6d8606ebca43442bb6bab78248f
Florent Latifi <120279421+FlorentLatifi@users.noreply.github.com>
2025-02-11 23:17:34 +0100

    A

--- db_connection.php
+++ db_connection.php
@@ -5,10 +5,11 @@ $username = "Diar";         // Përdoruesi i databazës
 $password = "Diar2005";     // Fjalëkalimi i databazës
 $dbname = "Projekti";       // Emri i databazës tuaj
 
-// Krijimi i lidhjes me MySQL
+// Krijimi i lidhjes me MySQL/MariaDB
 $conn = new mysqli($serverName, $username, $password, $dbname);
 
-// Kontrollo lidhjen
+// Kontrolloni nëse lidhja është e suksesshme
 if ($conn->connect_error) {
     die("Gabim gjatë lidhjes me bazën e të dhënave: " . $conn->connect_error);
 }
+//echo "Lidhja me bazën e të dhënave është bërë me sukses!";
